9e2869cfea6cf93584993af6692d8d0c3edd1ead
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-07 16:53:28 -0300

    Add PhotosAdapter

---
+++ app/src/main/java/com/example/albums/screens/albumdetail/PhotosAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.albums.screens.albumdetail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.albums.data.Photo
+import com.example.albums.databinding.PhotoListItemBinding
+
+
+class PhotosAdapter : ListAdapter<Photo, PhotosAdapter.ViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(private val binding: PhotoListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Photo) {
+            binding.photo = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = PhotoListItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Photo>() {
+        override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+}
