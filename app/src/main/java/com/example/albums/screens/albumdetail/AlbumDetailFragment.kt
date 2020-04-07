50f43f4b7300855da048d28787968b8316da2766
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-07 16:53:59 -0300

    Display images list in AlbumDetailFragment

--- app/src/main/java/com/example/albums/screens/albumdetail/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/albums/screens/albumdetail/AlbumDetailFragment.kt
@@ -1,13 +1,13 @@
 package com.example.albums.screens.albumdetail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.navArgs
-
-import com.example.albums.R
 import com.example.albums.databinding.FragmentAlbumDetailBinding
 
 /**
@@ -16,12 +16,22 @@ import com.example.albums.databinding.FragmentAlbumDetailBinding
 class AlbumDetailFragment : Fragment() {
 
     val args by navArgs<AlbumDetailFragmentArgs>()
+    val viewModel by viewModels<AlbumDetailViewModel>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAlbumDetailBinding.inflate(inflater)
         binding.album = args.selectedAlbum
+
+        val adapter = PhotosAdapter()
+        binding.albumDetailPhotoList.adapter = adapter
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+
         return binding.root
     }
 
