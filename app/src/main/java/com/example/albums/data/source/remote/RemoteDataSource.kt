aa1952776ee4d9f4fa30afc05acadec712e76795
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-08 19:54:00 -0300

    Create RemoteDataSource

---
+++ app/src/main/java/com/example/albums/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,8 @@
+package com.example.albums.data.source.remote
+
+import retrofit2.Retrofit
+import javax.inject.Inject
+import javax.inject.Named
+
+class RemoteDataSource @Inject constructor(api: Api) {
+}
