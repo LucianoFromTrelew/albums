ec19fd8f20ab4889a9eb555f057a661673976319
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-08 20:04:29 -0300

    Implement data fetching from remote API

--- app/src/main/java/com/example/albums/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/albums/data/source/remote/RemoteDataSource.kt
@@ -1,8 +1,29 @@
 package com.example.albums.data.source.remote
 
-import retrofit2.Retrofit
+import com.example.albums.data.Result
+import com.example.albums.data.Result.Error
+import com.example.albums.data.Result.Success
+import com.example.albums.data.domain.Album
+import com.example.albums.data.domain.Photo
+import com.example.albums.data.source.DataSource
 import javax.inject.Inject
-import javax.inject.Named
 
-class RemoteDataSource @Inject constructor(api: Api) {
+class RemoteDataSource @Inject constructor(private val api: Api) : DataSource {
+    override suspend fun getAlbums(shouldFetch: Boolean): Result<List<Album>> {
+        return try {
+            val albums = api.getAlbums().await()
+            Success(albums)
+        } catch (e: Exception) {
+            Error(e)
+        }
+    }
+
+    override suspend fun getPhotos(albumId: String, shouldFetch: Boolean): Result<List<Photo>> {
+        return try {
+            val photos = api.getPhotos(albumId).await()
+            Success(photos)
+        } catch (e: Exception) {
+            Error(e)
+        }
+    }
 }
