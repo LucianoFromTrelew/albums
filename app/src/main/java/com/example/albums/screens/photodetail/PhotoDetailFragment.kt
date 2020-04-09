545c66a0e4dee6a2111d0e9260c5a2cd860fbfc8
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-09 00:49:19 -0300

    Display selected photo

--- app/src/main/java/com/example/albums/screens/photodetail/PhotoDetailFragment.kt
+++ app/src/main/java/com/example/albums/screens/photodetail/PhotoDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.albums.databinding.FragmentPhotoDetailBinding
 
 /**
@@ -12,11 +13,14 @@ import com.example.albums.databinding.FragmentPhotoDetailBinding
  */
 class PhotoDetailFragment : Fragment() {
 
+    private val args by navArgs<PhotoDetailFragmentArgs>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentPhotoDetailBinding.inflate(inflater)
+        binding.photo = args.selectedPhoto
         return binding.root
     }
 
