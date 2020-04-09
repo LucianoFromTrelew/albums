0c5a8e89ce9859513a06148b6c412d1a0046d39d
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-09 00:46:34 -0300

    Migrate to Picasso

--- app/src/main/java/com/example/albums/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/albums/utils/BindingAdapters.kt
@@ -1,20 +1,11 @@
 package com.example.albums.utils
 
-import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.HttpException
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
 import com.example.albums.R
 import com.example.albums.screens.albumlist.ApiStatus
+import com.squareup.picasso.Picasso
 
 /**
  * Uses the Glide library to load an image by URL into an [ImageView]
@@ -22,40 +13,8 @@ import com.example.albums.screens.albumlist.ApiStatus
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        Glide.with(imgView.context)
-            .load(imgUri)
-            .apply(
-                RequestOptions()
-                    .placeholder(R.drawable.loading_animation)
-                    .error(R.drawable.ic_broken_image)
-            )
-            .listener(object: RequestListener<Drawable> {
-                override fun onLoadFailed(
-                    e: GlideException?,
-                    model: Any?,
-                    target: Target<Drawable>?,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    Log.d("imageUrl", e.toString())
-                    e?.run {
-                        logRootCauses("imageUrl")
-                    }
-                    return false
-                }
-
-                override fun onResourceReady(
-                    resource: Drawable?,
-                    model: Any?,
-                    target: Target<Drawable>?,
-                    dataSource: DataSource?,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    Log.d("imageUrl", "READY!")
-                    return false
-                }
-            })
-            .into(imgView)
+        Picasso.get().load(imgUrl).placeholder(R.drawable.loading_img)
+            .error(R.drawable.ic_broken_image).into(imgView)
     }
 }
 
