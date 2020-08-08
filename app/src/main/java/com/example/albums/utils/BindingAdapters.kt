599a86b6e19469f1b422793f31d8367778556296
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-08-08 13:16:33 -0300

    Remove stale comments from BindingAdapters

--- app/src/main/java/com/example/albums/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/albums/utils/BindingAdapters.kt
@@ -7,9 +7,6 @@ import com.example.albums.R
 import com.example.albums.screens.albumlist.ApiStatus
 import com.squareup.picasso.Picasso
 
-/**
- * Uses the Glide library to load an image by URL into an [ImageView]
- */
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
@@ -18,12 +15,6 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
     }
 }
 
-/**
- * This binding adapter displays the [MarsApiStatus] of the network request in an image view.  When
- * the request is loading, it displays a loading_animation.  If the request has an error, it
- * displays a broken image to reflect the connection error.  When the request is finished, it
- * hides the image view.
- */
 @BindingAdapter("apiStatus")
 fun bindStatus(statusImageView: ImageView, status: ApiStatus) {
     when (status) {
